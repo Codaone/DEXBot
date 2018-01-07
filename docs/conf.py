35718936ffd2d123cbf2439bf67a71aa5c7da2ca
Ian Haywood <ian@haywood.id.au>
2018-01-07 11:59:38 +1100

    fixes to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# StakeMachine documentation build configuration file, created by
+# DEXBot documentation build configuration file, created by
 # sphinx-quickstart on Fri May 27 11:21:25 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -202,7 +202,7 @@ html_theme = 'alabaster'
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'StakeMachinedoc'
+htmlhelp_basename = 'DEXBotdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -224,8 +224,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'StakeMachine.tex', 'StakeMachine Documentation',
-   'Fabian Schuh', 'manual'),
+  (master_doc, 'DEXBot.tex', 'DEXBot Documentation',
+   'Fabian Schuh, DEXBot developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'dexbot', 'StakeMachine Documentation',
+    (master_doc, 'dexbot', 'DEXBot Documentation',
      [author], 1)
 ]
 
@@ -268,8 +268,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'StakeMachine', 'StakeMachine Documentation',
-   author, 'StakeMachine', 'One line description of project.',
+  (master_doc, 'DEXBot', 'DEXBot Documentation',
+   author, 'DEXBot', 'A bot framework for the BtiShares DEX',
    'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
-Welcome to StakeMachine's documentation!
-========================================
+Welcome to DEXBot's documentation!
+==================================
 
 Basics
 ------
