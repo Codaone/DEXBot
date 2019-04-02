c49a2049ee6151787aab6411071a2b6159b0880f
bitProfessor <jackingyang@qq.com>
2019-04-03 07:46:04 +0800

    Update test_relative_orders.py

--- tests/test_dexbot/test_strategies/test_relative_orders.py
+++ tests/test_dexbot/test_strategies/test_relative_orders.py
@@ -1,52 +1,16 @@
 import pytest
 from dexbot.strategies.relative_orders import Strategy
+from bitshares import BitShares
+from bitshares.notify import Notify
 import logging
 logging.basicConfig(format='%(asctime)s %(funcName)s %(lineno)d  : %(message)s',
                     level=logging.WARNING)
 class Test_Strategy:
     def setup_class(self):
-        from bitshares import BitShares
-        from bitshares.account import Account
-        nodeList=["wss://bts.open.icowallet.net/ws",
-                "wss://bitshares.dacplay.org/ws",
-                "wss://ws.gdex.top",
-                "wss://api.bts.ai"
-                ]
-        # nodeList=["wss://node.testnet.bitshares.eu"]
-        self.bitShares = BitShares(nodeList)#,nobroadcast=True)
-        self.bitShares.account='bts0207'
-        # self.account=Account('bts0207',bitshares_instance=self.bitShares)
-        # # self.bitShares.account='dexbot-test1'
-        # # self.account=Account('dexbot-test1',bitshares_instance=self.bitShares)    
         TEST_CONFIG = {
             'node':'wss://bts.open.icowallet.net/ws',
             'workers':{
-        #         'dexbot-test1':{
-        #             'account': 'dexbot-test1',
-        #             'amount': 1.0,
-        #             'center_price': 0.3,
-        #             'center_price_depth': 0.0,
-        #             'center_price_dynamic': True,
-        #             'center_price_offset': False,
-        #             'custom_expiration': False,
-        #             'dynamic_spread': False,
-        #             'dynamic_spread_factor': 1.0,
-        #             'expiration_time': 157680000.0,
-        #             'external_feed': False,
-        #             'external_price_source': 'null',
-        #             'fee_asset': 'TEST',
-        #             'manual_offset': 0.0,
-        #             'market': 'TEST/BTWTY',
-        #             'market_depth_amount': 0.0,
-        #             'module': 'dexbot.strategies.relative_orders',
-        #             'partial_fill_threshold': 30.0,
-        #             'price_change_threshold': 2.0,
-        #             'relative_order_size': False,
-        #             'reset_on_partial_fill': True,
-        #             'reset_on_price_change': False,
-        #             'spread': 5.0
-        #         },
-            'bts0207':{
+                'bts0207':{
                     'account': 'bts0207',
                     'amount': 1,
                     'center_price': 0.3,
@@ -71,54 +35,22 @@ class Test_Strategy:
                     'reset_on_price_change': False,
                     'spread': 5.0
                 }
-        #     # 'bts0207':{
-        #     #         'account': 'bts0207',
-        #     #         'amount': 1,#order size:1.amount=0 ok.2.amount=None error 3.amount=9999999 error
-        #     #         'center_price': 0,#
-        #     #         'center_price_depth': 0,
-        #     #         'center_price_dynamic': True,
-        #     #         'center_price_offset': True,
-        #     #         'custom_expiration': False,
-        #     #         'dynamic_spread': False,
-        #     #         'dynamic_spread_factor': 0,
-        #     #         'expiration_time': 157680000.0,
-        #     #         'external_feed': False,
-        #     #         'external_price_source': 'null',
-        #     #         'fee_asset': 'BTS',
-        #     #         'manual_offset': 0.0,
-        #     #         'market': 'BTS/CNY',
-        #     #         'market_depth_amount': 0.0,
-        #     #         'module': 'dexbot.strategies.relative_orders',
-        #     #         'partial_fill_threshold': 30.0,
-        #     #         'price_change_threshold': 2.0,
-        #     #         'relative_order_size': False,
-        #     #         'reset_on_partial_fill': True,
-        #     #         'reset_on_price_change': False,
-        #     #         'spread': 5.0
-        #     #     }
-            }   
-        }   
+            }
+        } 
+        nodeList=["wss://bts.open.icowallet.net/ws",
+                "wss://bitshares.dacplay.org/ws",
+                "wss://ws.gdex.top",
+                "wss://api.bts.ai"
+                ]
+        self.bitShares = BitShares(nodeList)#,nobroadcast=True)
         if self.bitShares.wallet.locked():
             self.bitShares.wallet.unlock('123')
-        self.relative_strategy=Strategy(name='bts0207',config=TEST_CONFIG,bitshares_instance=self.bitShares)    
-        # for e in self.relative_strategy.__dict__.items():
-        #         logging.warning(e)
+        self.relative_strategy=Strategy(name='bts0207',config=TEST_CONFIG,bitshares_instance=self.bitShares) 
     def teardown_class(self):
         pass
     def setup_method(self):
-        # from bitshares import BitShares
-        # from bitshares.account import Account
-        #path='/Users/jacking/Documents/GitHub/stakemachine/config.yml'
-
-        # print(self.relative_strategy.get_market_center_price())
-        # if self.bitShares.wallet.locked():
-        #     self.bitShares.wallet.unlock('123')
-        # self.relative_strategy=Strategy(name='dexbot-test1',config=TEST_CONFIG,bitshares_instance=self.bitShares)    
-        # self.assertEqual(Account('dexbot-test1',bitshares_instance=self.bitShares),self.relative_strategy.account,'error')
-        # self.relative_strategy=Strategy(name='bts0207',config=TEST_CONFIG,bitshares_instance=self.bitShares)    
-        # self.assertEqual(Account('bts0207',bitshares_instance=self.bitShares),Account(self.relative_strategy.account,bitshares_instance=self.bitShares),'error')
-        # print('test node:There is
-        pass
+        if self.bitShares.wallet.locked():
+            self.bitShares.wallet.unlock('123')
     def teardown_method(self):
         pass
     def test_configure(self):
@@ -128,7 +60,6 @@ class Test_Strategy:
         logging.warning(account)
         assert account in cf
     def test_configure_details(self):
-        # from dexbot.strategies.base import DetailElement
         cf_details=Strategy.configure_details()
         logging.warning(cf_details)
         assert cf_details==[]
@@ -137,25 +68,11 @@ class Test_Strategy:
         logging.warning(r)
         assert r==None
     def test_tick(self):
-        r=self.relative_strategy.tick('d')
+        r=self.relative_strategy.tick('0228fec41a799ec45c218cb441ae249cef1bbcb2')
         logging.warning(r)
         assert None==r
     def test_amount_to_sell(self):
-        # print(self.relative_strategy.worker)
-        # amount=1
-        # c=self.relative_strategy.market['quote']
-        # d=self.relative_strategy.market['quote']['precision']
         self.relative_strategy.sell_price=1
-        # print(10 ** -5)
-        # print(c,d)
-        # from bitshares.asset import Asset
-        # aa=Asset('1.3.0')
-        # print(aa.symbol)
-        # print(dict(aa))
-        # print(aa.settlements)
-        # if (amount < 2 * 10 ** -d or
-        #         amount * self.sell_price < 2 * 10 ** -d):
-        #     amount = 0
         amount_to_sell=self.relative_strategy.amount_to_sell
         logging.warning(amount_to_sell)
     def test_amount_to_buy(self):
@@ -187,7 +104,18 @@ class Test_Strategy:
         logging.warning(r)
         assert r==None
 
+
 #--------------------------test------------------------
 if __name__=='__main__':
+    # a=Test_Strategy()
+    # notify=Notify(
+    #     markets=['BTS:CNY'],
+    #     accounts=['BTS0207'],
+    #     on_market=a.test_tick,
+    #     # on_account=self.on_account,
+    #     # on_block=self.on_block,
+    #     # bitshares_instance=self.bitshares
+    # )
+    # notify.listen()
     path='/Users/jacking/Documents/GitHub/env/DEXBot/tests/test_dexbot/test_strategies/'
     pytest.main([path+'test_relative_orders.py'])
