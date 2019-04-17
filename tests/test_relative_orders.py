57d33580b29facbfa72d75dd4541766f9d979d01
bitProfessor <bitprofessor@163.com>
2019-04-17 20:28:42 +0800

    Update test_relative_orders.py

--- tests/test_relative_orders.py
+++ tests/test_relative_orders.py
@@ -1,6 +1,4 @@
-import pytest
 from dexbot.strategies.relative_orders import Strategy
-from dexbot.strategies.config_parts.relative_config import ConfigElement
 from fixtures import fixture_data
 import logging
 import math
@@ -27,40 +25,48 @@ class Test_Strategy:
         pass
 
     def test_configure(self):
-        account = ConfigElement(
-            key='account', type='string', default='', title='Account',
-            description='BitShares account name for the bot to operate with',
-            extra=''
-        )
-        cf = Strategy.configure()
-        logging.info(account)
-        assert account in cf
+        logging.info(self.relative_strategy.configure())
+        account = self.TEST_CONFIG['workers']['worker 1']['account']
+        conditon = ('dexbot-test1' != account)
+
+        if conditon:
+            raise AssertionError('account error!')
 
     def test_configure_details(self):
-        cf_details = Strategy.configure_details()
+        cf_details = self.relative_strategy.configure_details()
         logging.info(cf_details)
-        assert cf_details == []
+        conditon = (cf_details == [])
+        if not conditon:
+            raise AssertionError('Not empty value！')
 
     def test_error(self):
+        '''Event method return None'''
         r = self.relative_strategy.error()
         logging.info(r)
-        assert r is None
+        if r is not None:
+            raise AssertionError('Event method return None!')
 
     def test_tick(self):
+        '''Event method return None'''
         r = self.relative_strategy.tick(
             '0228fec41a799ec45c218cb441ae249cef1bbcb2')
         logging.info(r)
-        assert r is None
+        if r is not None:
+            raise AssertionError('Event method return None!')
 
     def test_amount_to_sell(self):
         logging.info('sell_price:{}'.format(self.relative_strategy.sell_price))
-        assert self.relative_strategy.sell_price is None
+        r = self.relative_strategy.sell_price
+        if r is not None:
+            raise AssertionError('The method should have no return value!')
 
         self.relative_strategy.calculate_order_prices()
         amount_to_sell = self.relative_strategy.amount_to_sell
         logging.info('amount_to_sell:{}'.format(amount_to_sell))
 
-        assert amount_to_sell == self.TEST_CONFIG['workers']['worker 1']['amount']
+        amount = self.TEST_CONFIG['workers']['worker 1']['amount']
+        if amount_to_sell != amount:
+            raise AssertionError('order size wrong!')
 
     def test_amount_to_buy(self):
         self.relative_strategy.calculate_order_prices()
@@ -69,13 +75,16 @@ class Test_Strategy:
         amount_to_buy = self.relative_strategy.amount_to_buy
         logging.info('amount_to_buy:{}'.format(amount_to_buy))
 
-        assert amount_to_buy == self.TEST_CONFIG['workers']['worker 1']['amount']
+        amount_to_buy_cf = self.TEST_CONFIG['workers']['worker 1']['amount']
+        if amount_to_buy != amount_to_buy_cf:
+            raise AssertionError('order size wrong!')
 
     def test_calculate_order_prices(self):
         # no return
         calculate_prices = self.relative_strategy.calculate_order_prices()
         logging.info(calculate_prices)
-        assert calculate_prices is None
+        if calculate_prices is not None:
+            raise AssertionError('The method should have no return value!')
 
         buy_price = self.relative_strategy.buy_price
         sell_price = self.relative_strategy.sell_price
@@ -86,7 +95,8 @@ class Test_Strategy:
         logging.info('center_price:{}'.format(center_price))
         logging.info('spread:{}'.format(spread))
 
-        assert self.relative_strategy.center_price == center_price
+        if self.relative_strategy.center_price != center_price:
+            raise AssertionError('Center price calculation error!')
 
         buy_price_ca = center_price / math.sqrt(1 + spread)
         logging.info('buy_price_ca:{}'.format(buy_price_ca))
@@ -94,8 +104,10 @@ class Test_Strategy:
         sell_price_ca = center_price * math.sqrt(1 + spread)
         logging.info('sell_price_ca:{}'.format(sell_price_ca))
 
-        assert buy_price == buy_price_ca
-        assert sell_price == sell_price_ca
+        if buy_price != buy_price_ca:
+            raise AssertionError('buy_price calculation error!')
+        if sell_price != sell_price_ca:
+            raise AssertionError('sell_price calculation error!')
 
     def test_update_orders(self):
         self.relative_strategy.update_orders()
@@ -104,9 +116,11 @@ class Test_Strategy:
         market = Market(self.TEST_CONFIG['workers']['worker 1']['market'])
         for o in market.accountopenorders():
             if o.get('for_sale') == 1:
-                assert o.get('price') == 3.2530904359141184
+                if o.get('price') != 3.2530904359141184:
+                    raise AssertionError('Order Price Error！')
             else:
-                assert o.get('price') == 0.2927
+                if o.get('price') != 0.2927:
+                    raise AssertionError('Order Price Error！')
 
     def test_calculate_center_price(self):
         from bitshares.market import Market
@@ -117,7 +131,8 @@ class Test_Strategy:
         cp = highest_bid * math.sqrt(lowest_ask / highest_bid)
         center_price = self.relative_strategy.calculate_center_price()
 
-        assert cp == center_price
+        if cp != center_price:
+            raise AssertionError('Center price calculation error!')
 
     def test_calculate_asset_offset(self):
         from bitshares.market import Market
@@ -165,7 +180,8 @@ class Test_Strategy:
             center_price=center_price, order_ids=[], spread=spread)
 
         logging.info(calculate_asset_offset)
-        assert calculate_asset_offset == r
+        if calculate_asset_offset != r:
+            raise AssertionError('Calculate asset offset wrong!')
 
     def test_calculate_manual_offset(self):
         center_price = 1
@@ -175,7 +191,8 @@ class Test_Strategy:
             center_price=center_price, manual_offset=manual_offset)
         logging.info(calculate_offset)
 
-        assert calculate_offset == center_price * (1 + manual_offset)
+        if calculate_offset != center_price * (1 + manual_offset):
+            raise AssertionError('Calculate manual offset wrong!')
 
         manual_offset = -0.1
 
@@ -183,7 +200,16 @@ class Test_Strategy:
             center_price=center_price, manual_offset=manual_offset)
         logging.info(calculate_offset)
 
-        assert calculate_offset == center_price / (1 + abs(manual_offset))
+        if calculate_offset != center_price / (1 + abs(manual_offset)):
+            raise AssertionError('Calculate manual offset wrong!')
 
     def test_check_orders(self):
         self.relative_strategy.check_orders()
+
+
+if __name__ == '__main__':
+    import os
+    import pytest
+    cur_dir = os.path.dirname(__file__)
+    test_file = os.path.join(cur_dir, 'test_relative_orders.py')
+    pytest.main(['--capture=no', test_file])
