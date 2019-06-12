4e45d8067938ce46f3cecaff015cc07571500e55
bitProfessor <bitProfessor@163.com>
2019-06-12 17:45:07 +0800

    docker test bitshares_feed

---
+++ tests/pricefeeds/conftest.py
@@ -0,0 +1,67 @@
+import pytest
+import time
+
+from dexbot.pricefeeds.bitshares_feed import BitsharesPriceFeed
+from bitshares.market import Market
+
+@pytest.fixture(scope='module')
+def assets(create_asset):
+    create_asset('MYBASE', 0)
+    create_asset('MYQUOTE', 5)
+
+
+@pytest.fixture(scope='session')
+def account_name():
+    """ Fixture to share  name
+    """
+    return 'bf'
+
+
+@pytest.fixture(scope='module')
+def market(bitshares, accounts, account_name):
+    """ Create  market
+    """
+    market = Market('MYQUOTE/MYBASE', bitshares_instance=bitshares, account=account_name)
+    return market
+
+
+@pytest.fixture(scope='module')
+def accounts(assets, prepare_account, account_name):
+    prepare_account({'MYBASE': 20000, 'MYQUOTE': 5000, 'TEST': 10000}, account=account_name)
+
+
+@pytest.fixture(scope='module')
+def base_bitshares_feed(bitshares, market):
+    """ create BitsharesPriceFeed object from dexbot.pricefeeds.bitshares_feed.py
+    """
+
+    def _base_bitshares_feed():
+        bf = BitsharesPriceFeed(
+            market=market, bitshares_instance=bitshares)
+        return bf
+
+    return _base_bitshares_feed
+
+
+@pytest.fixture(scope='module')
+def bitshares_feed(base_bitshares_feed):
+    """ create BitsharesPriceFeed object from dexbot.pricefeeds.bitshares_feed.py
+    """
+    bf = base_bitshares_feed()
+    return bf
+
+
+@pytest.fixture(scope='module')
+def orders1(market,account_name):
+    """ Create some orders
+    """
+    os = []
+    for i in range(1, 2):
+        o = market.buy(1 + i, 1, returnOrderId=True, account=account_name)
+        os.append(o['orderid'])
+        o = market.sell(2 + i, 1, returnOrderId=True, account=account_name)
+        os.append(o['orderid'])
+
+    yield
+    market.cancel(os, account=account_name)
+    time.sleep(1.1)
--- tests/pricefeeds/test_bitshares_feed.py
+++ tests/pricefeeds/test_bitshares_feed.py
@@ -1,232 +1,229 @@
-from dexbot.pricefeeds.bitshares_feed import BitsharesPriceFeed
-from bitshares.bitshares import BitShares
-from grapheneapi.exceptions import RPCError
-from bitshares.market import Market
-import logging
-import os
-import pytest
 import math
 import operator
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(funcName)s %(lineno)d  : %(message)s'
-)
+import pytest
+
+from grapheneapi.exceptions import RPCError
+from bitshares.asset import Asset
+from bitshares.account import Account
 
 
-class Test_PriceFeed:
-    def setup_class(self):
-        test_config = {'node': "wss://bts.open.icowallet.net/ws",
-                       'market': 'CNY:BTS'}
-        assert test_config['node'] == "wss://bts.open.icowallet.net/ws"
-        self.bts = BitShares(node=test_config['node'])
-        assert test_config['market'] == 'CNY:BTS'
-        self.market = Market(test_config['market'])
+def test_balance(bitshares, accounts, account_name):
+    a = Account(account_name, bitshares_instance=bitshares)
+    assert a.balance('MYBASE') == 20000
+    assert a.balance('MYQUOTE') == 5000
+    assert a.balance('TEST') == 10000
 
-        self.pf = BitsharesPriceFeed(
-            market=self.market, bitshares_instance=self.bts)
 
-        assert self.bts.info == self.pf.bitshares.info
-        assert self.market == self.pf.market
-        assert self.pf.fetch_depth == 8
-        assert self.pf.ticker == self.pf.market.ticker
+def test_asset_base(bitshares, assets):
+    a = Asset('MYBASE', full=True, bitshares_instance=bitshares)
+    assert a['dynamic_asset_data']['current_supply'] > 1000
 
-    def teardown_class(self):
-        pass
 
-    def setup_method(self):
-        pass
+def test_asset_quote(bitshares, assets):
+    a = Asset('MYQUOTE', full=True, bitshares_instance=bitshares)
+    assert a['dynamic_asset_data']['current_supply'] > 1000
 
-    def teardown_method(self):
-        pass
 
-    def test_get_ticker(self):
-        ticker = self.pf.ticker
-        assert ticker() == self.market.ticker()
+def test_market(bitshares_feed, market):
+    assert market == bitshares_feed.market
+    assert bitshares_feed.fetch_depth == 8
+    assert bitshares_feed.ticker == bitshares_feed.market.ticker
 
-    def test_get_limit_orders(self):
-        # test default=1
-        mkt_orders = self.pf.get_limit_orders()
-        assert len(mkt_orders) == 2
-        # test depth=0
-        mkt_orders = self.pf.get_limit_orders(depth=0)
-        assert len(mkt_orders) == 0
-        # test depth=-1
-        with pytest.raises(RPCError):
-            mkt_orders = self.pf.get_limit_orders(depth=-1)
-        # bitshares Assert limit <= 300
-        mkt_orders = self.pf.get_limit_orders(depth=300)
-        assert len(mkt_orders) == 600
-        # test depth=10000
-        with pytest.raises(RPCError):
-            mkt_orders = self.pf.get_limit_orders(depth=10000)
 
-    def test_get_orderbook_orders(self):
-        # test default=1
-        orderbook = self.pf.get_orderbook_orders()
-        assert len(orderbook['bids']) == 1
-        assert len(orderbook['asks']) == 1
-        # test depth=-1
-        with pytest.raises(RPCError):
-            orderbook = self.pf.get_orderbook_orders(depth=-1)
-        # test depth=0
-        orderbook = self.pf.get_orderbook_orders(depth=0)
-        assert len(orderbook['bids']) == 0
-        assert len(orderbook['asks']) == 0
-        # test depth=50
-        orderbook = self.pf.get_orderbook_orders(depth=50)
-        assert len(orderbook['bids']) == 50
-        assert len(orderbook['asks']) == 50
-        # test depth=60 orderbook() call has hard-limit of depth=50
-        with pytest.raises(RPCError):
-            orderbook = self.pf.get_orderbook_orders(depth=60)
+def test_get_ticker(bitshares_feed):
+    bitshares_feed.ticker
 
-    def test_filter_buy_orders(self):
-        buy_orders = self.pf.get_market_buy_orders(depth=10)
-        price_list = []
-        for o in buy_orders:
-            if o['base']['symbol'] == self.market['base']['symbol']:
-                price_list.append(o['price'])
-        asc = sorted(price_list, reverse=False)
-        desc = sorted(price_list, reverse=True)
 
-        asc_orders = self.pf.filter_buy_orders(buy_orders, sort='ASC')
-        asc_orders_prices = []
-        for o in asc_orders:
-            asc_orders_prices.append(o['price'])
+def test_get_limit_orders(bitshares_feed, orders1):
+    # test default=1
+    mkt_orders = bitshares_feed.get_limit_orders()
+    assert len(mkt_orders) == 2
+    # test depth=0
+    mkt_orders = bitshares_feed.get_limit_orders(depth=0)
+    assert len(mkt_orders) == 0
+    # test depth=-1
+    with pytest.raises(RPCError):
+        mkt_orders = bitshares_feed.get_limit_orders(depth=-1)
+    # test depth=10000
+    with pytest.raises(RPCError):
+        mkt_orders = bitshares_feed.get_limit_orders(depth=10000)
 
-        desc_orders = self.pf.filter_buy_orders(buy_orders, sort='DESC')
-        desc_orders_prices = []
-        for o in desc_orders:
-            desc_orders_prices.append(o['price'])
 
-        assert asc == asc_orders_prices
-        assert desc == desc_orders_prices
+def test_get_orderbook_orders(bitshares_feed, orders1):
+    # test default=1
+    orderbook = bitshares_feed.get_orderbook_orders()
+    assert len(orderbook['bids']) == 1
+    assert len(orderbook['asks']) == 1
+    # test depth=-1
+    with pytest.raises(RPCError):
+        orderbook = bitshares_feed.get_orderbook_orders(depth=-1)
+    # test depth=0
+    orderbook = bitshares_feed.get_orderbook_orders(depth=0)
+    assert len(orderbook['bids']) == 0
+    assert len(orderbook['asks']) == 0
 
-        assert operator.eq(asc, asc_orders_prices)
-        assert operator.eq(desc, desc_orders_prices)
+    # test depth=60 orderbook() call has hard-limit of depth=50
+    with pytest.raises(RPCError):
+        orderbook = bitshares_feed.get_orderbook_orders(depth=60)
 
-    def test_filter_sell_orders(self):
-        sell_orders = self.pf.get_market_sell_orders(depth=10)
 
-        price_list = []
-        for o in sell_orders:
-            if o['base']['symbol'] != self.market['base']['symbol']:
-                price_list.append(o['price'])
-        asc = sorted(price_list, reverse=False)
-        desc = sorted(price_list, reverse=True)
-
-        asc_orders = self.pf.filter_sell_orders(sell_orders, sort='ASC')
-        asc_orders_prices = []
-        for o in asc_orders:
-            asc_orders_prices.append(o['price'])
-
-        desc_orders = self.pf.filter_sell_orders(sell_orders, sort='DESC')
-        desc_orders_prices = []
-        for o in desc_orders:
-            desc_orders_prices.append(o['price'])
-
-        assert asc == asc_orders_prices
-        assert desc == desc_orders_prices
-
-        assert operator.eq(asc, asc_orders_prices)
-        assert operator.eq(desc, desc_orders_prices)
-
-    def test_get_highest_market_buy_order(self):
-        buy_orders = self.pf.get_market_buy_orders(depth=10)
-        price_list = []
-        for o in buy_orders:
+def test_filter_buy_orders(bitshares_feed, orders1):
+    buy_orders = bitshares_feed.get_market_buy_orders(depth=10)
+    price_list = []
+    for o in buy_orders:
+        if o['base']['symbol'] == bitshares_feed.market['base']['symbol']:
             price_list.append(o['price'])
-        max_price = max(price_list)
+    asc = sorted(price_list, reverse=False)
+    desc = sorted(price_list, reverse=True)
 
-        highest = self.pf.get_highest_market_buy_order(buy_orders)
+    asc_orders = bitshares_feed.filter_buy_orders(buy_orders, sort='ASC')
+    asc_orders_prices = []
+    for o in asc_orders:
+        asc_orders_prices.append(o['price'])
 
-        assert max_price == highest['price']
+    desc_orders = bitshares_feed.filter_buy_orders(buy_orders, sort='DESC')
+    desc_orders_prices = []
+    for o in desc_orders:
+        desc_orders_prices.append(o['price'])
 
-    def test_get_lowest_market_sell_order(self):
-        sell_orders = self.pf.get_market_sell_orders(depth=10)
-        price_list = []
-        for o in sell_orders:
+    assert asc == asc_orders_prices
+    assert desc == desc_orders_prices
+
+    assert operator.eq(asc, asc_orders_prices)
+    assert operator.eq(desc, desc_orders_prices)
+
+@pytest.mark.xfail(reason='order.invert()-->graphenecommon/blockchainobject.py:262 self.refresh()')
+def test_filter_sell_orders(bitshares_feed, orders1):
+    sell_orders = bitshares_feed.get_market_sell_orders(depth=10)
+
+    price_list = []
+    for o in sell_orders:
+        if o['base']['symbol'] != bitshares_feed.market['base']['symbol']:
             price_list.append(o['price'])
-        min_price = min(price_list)
+    asc = sorted(price_list, reverse=False)
+    desc = sorted(price_list, reverse=True)
 
-        lowest = self.pf.get_lowest_market_sell_order(sell_orders)
+    asc_orders = bitshares_feed.filter_sell_orders(sell_orders, sort='ASC')
+    asc_orders_prices = []
+    for o in asc_orders:
+        asc_orders_prices.append(o['price'])
 
-        assert min_price == lowest['price']
+    desc_orders = bitshares_feed.filter_sell_orders(sell_orders, sort='DESC')
+    desc_orders_prices = []
+    for o in desc_orders:
+        desc_orders_prices.append(o['price'])
 
-    def test_get_market_buy_orders(self):
-        buy_orders = self.pf.get_market_buy_orders(depth=10)
-        for o in buy_orders:
-            assert o['base']['symbol'] == self.market['base']['symbol']
+    assert asc == asc_orders_prices
+    assert desc == desc_orders_prices
 
-    def test_get_market_sell_orders(self):
-        sell_orders = self.pf.get_market_sell_orders(depth=10)
-        for o in sell_orders:
-            assert o['base']['symbol'] == self.market['base']['symbol']
+    assert operator.eq(asc, asc_orders_prices)
+    assert operator.eq(desc, desc_orders_prices)
 
-    def test_get_market_buy_price(self):
-        highestBid = self.market.ticker().get('highestBid')
-        mkt_buy_price = self.pf.get_market_buy_price(
-            quote_amount=0, base_amount=0)
 
-        assert float(highestBid) == mkt_buy_price
+def test_get_highest_market_buy_order(bitshares_feed, orders1):
+    buy_orders = bitshares_feed.get_market_buy_orders(depth=10)
+    price_list = []
+    for o in buy_orders:
+        price_list.append(o['price'])
+    max_price = max(price_list)
 
-        mkt_buy_price = self.pf.get_market_buy_price(
-            quote_amount=10, base_amount=1)
+    highest = bitshares_feed.get_highest_market_buy_order(buy_orders)
 
-    def test_get_market_sell_price(self):
-        lowestAsk = self.market.ticker().get('lowestAsk')
-        mkt_sell_price = self.pf.get_market_sell_price(
-            quote_amount=0, base_amount=0)
+    assert max_price == highest['price']
 
-        assert float(lowestAsk) == mkt_sell_price
+@pytest.mark.xfail(reason='order.invert()-->graphenecommon/blockchainobject.py:262 self.refresh()')
+def test_get_lowest_market_sell_order(bitshares_feed, orders1):
+    sell_orders = bitshares_feed.get_market_sell_orders(depth=10)
+    price_list = []
+    for o in sell_orders:
+        price_list.append(o['price'])
+    min_price = min(price_list)
 
-        mkt_sell_price = self.pf.get_market_sell_price(
-            quote_amount=10, base_amount=1)
+    lowest = bitshares_feed.get_lowest_market_sell_order(sell_orders)
 
-    def test_get_market_center_price(self):
-        lowestAsk = self.market.ticker().get('lowestAsk')
-        highestBid = self.market.ticker().get('highestBid')
+    assert min_price == lowest['price']
+
+
+def test_get_market_buy_orders(bitshares_feed, orders1):
+    buy_orders = bitshares_feed.get_market_buy_orders(depth=10)
+    for o in buy_orders:
+        assert o['base']['symbol'] == bitshares_feed.market['base']['symbol']
+
+@pytest.mark.xfail(reason='order.invert()-->graphenecommon/blockchainobject.py:262 self.refresh()')
+def test_get_market_sell_orders(bitshares_feed, orders1):
+    os = bitshares_feed.get_limit_orders()
+    for order in os:
+        if order['base']['symbol'] == bitshares_feed.market['quote']['symbol']:
+            assert order['base']['symbol'] == bitshares_feed.market['quote']['symbol']
+
+    sell_orders = bitshares_feed.get_market_sell_orders(depth=1)
+    for o in sell_orders:
+        assert o['base']['symbol'] == bitshares_feed.market['quote']['symbol']
+
+
+def test_get_market_buy_price(bitshares_feed):
+    highestBid = bitshares_feed.market.ticker().get('highestBid')
+    mkt_buy_price = bitshares_feed.get_market_buy_price(
+        quote_amount=0, base_amount=0)
+
+    assert float(highestBid) == mkt_buy_price
+
+
+def test_get_market_sell_price(bitshares_feed):
+    lowestAsk = bitshares_feed.market.ticker().get('lowestAsk')
+    mkt_sell_price = bitshares_feed.get_market_sell_price(
+        quote_amount=0, base_amount=0)
+
+    assert float(lowestAsk) == mkt_sell_price
+
+
+def test_get_market_center_price(bitshares_feed, orders1):
+    lowestAsk = bitshares_feed.market.ticker().get('lowestAsk')
+    highestBid = bitshares_feed.market.ticker().get('highestBid')
+    if highestBid == 0:
+        cp = None
+    else:
         cp = highestBid * math.sqrt(lowestAsk / highestBid)
-        center_price = self.pf.get_market_center_price(
-            base_amount=0, quote_amount=0, suppress_errors=False)
+        cp = float(cp)
+    center_price = bitshares_feed.get_market_center_price(
+        base_amount=0, quote_amount=0, suppress_errors=False)
+    assert cp == center_price
 
-        assert float(cp) == center_price
 
-    def test_get_market_spread(self):
-        lowestAsk = self.market.ticker().get('lowestAsk')
-        highestBid = self.market.ticker().get('highestBid')
+def test_get_market_spread(bitshares_feed):
+    lowestAsk = bitshares_feed.market.ticker().get('lowestAsk')
+    highestBid = bitshares_feed.market.ticker().get('highestBid')
+    if highestBid == 0:
+        spread = None
+    else:
         spread = lowestAsk / highestBid - 1
-        mkt_spread = self.pf.get_market_spread(quote_amount=0, base_amount=0)
+        spread = float(spread)
+    mkt_spread = bitshares_feed.get_market_spread(quote_amount=0, base_amount=0)
 
-        assert spread == mkt_spread
-
-    def test_sort_orders_by_price(self):
-        buy_orders = self.pf.get_market_buy_orders(depth=10)
-        price_list = []
-        for o in buy_orders:
-            if o['base']['symbol'] == self.market['base']['symbol']:
-                price_list.append(o['price'])
-        asc = sorted(price_list, reverse=False)
-        desc = sorted(price_list, reverse=True)
-
-        asc_orders = self.pf.sort_orders_by_price(buy_orders, sort='ASC')
-        asc_orders_prices = []
-        for o in asc_orders:
-            asc_orders_prices.append(o['price'])
-
-        desc_orders = self.pf.sort_orders_by_price(buy_orders, sort='DESC')
-        desc_orders_prices = []
-        for o in desc_orders:
-            desc_orders_prices.append(o['price'])
-
-        assert asc == asc_orders_prices
-        assert desc == desc_orders_prices
-
-        assert operator.eq(asc, asc_orders_prices)
-        assert operator.eq(desc, desc_orders_prices)
+    assert spread == mkt_spread
 
 
-if __name__ == '__main__':
-    cur_dir = os.path.dirname(__file__)
-    test_file = os.path.join(cur_dir, 'test_bitshares_feed.py')
-    pytest.main(['--capture=no', test_file])
+def test_sort_orders_by_price(bitshares_feed, orders1):
+    buy_orders = bitshares_feed.get_market_buy_orders(depth=10)
+    price_list = []
+    for o in buy_orders:
+        if o['base']['symbol'] == bitshares_feed.market['base']['symbol']:
+            price_list.append(o['price'])
+    asc = sorted(price_list, reverse=False)
+    desc = sorted(price_list, reverse=True)
+
+    asc_orders = bitshares_feed.sort_orders_by_price(buy_orders, sort='ASC')
+    asc_orders_prices = []
+    for o in asc_orders:
+        asc_orders_prices.append(o['price'])
+
+    desc_orders = bitshares_feed.sort_orders_by_price(buy_orders, sort='DESC')
+    desc_orders_prices = []
+    for o in desc_orders:
+        desc_orders_prices.append(o['price'])
+
+    assert asc == asc_orders_prices
+    assert desc == desc_orders_prices
+
+    assert operator.eq(asc, asc_orders_prices)
+    assert operator.eq(desc, desc_orders_prices)
